85613552d6c04a651399e94a63a25d0d44cdfb62
Guillermo Acquistapace <guillermoacq@gmail.com>
2023-10-18 10:48:02 -0300

    Added initial program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,8 +6,25 @@ namespace CompAndDel
 {
     class Program
     {
+        private const string SAVED_PICTURE_PATH = @"editedPhoto.jpg";
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"beer.jpg");
+
+            IPicture filteredPicture = Send(picture, new FilterBlurConvolution());
+            provider.SavePicture(picture, SAVED_PICTURE_PATH);
+        }
+
+        private static IPicture Send(IPicture picture, IFilter filter)
+        {
+            PipeSerial pipeSerial;
+            PipeSerial pipeSerial2;
+            
+            pipeSerial2 = new PipeSerial(filter, null);
+            pipeSerial = new PipeSerial(filter, pipeSerial2);
+
+            return picture;
         }
     }
 }
